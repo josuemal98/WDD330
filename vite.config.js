1238cb5f07c0cde56cc3d72b0fe13809875f1ab9
josuemal98 <jmaldonadofuentes@byupathway.edu>
2026-09-22 21:11:31 -0600

    viteeee

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src",
+  base: "/WDD330/",
+  root: "src/",
 
   build: {
     outDir: "../dist",
