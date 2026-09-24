269ecc4c762d2e84375cdd76092bbc677ab678ad
josuemal98 <jmaldonadofuentes@byupathway.edu>
2026-09-23 20:53:35 -0600

    complete checkout process

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices("tents");
 
 const element = document.querySelector(".product-list");
 
